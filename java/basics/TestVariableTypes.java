5b2ff604d3a0b059b89509b70a5619a8512ec12b
1lch2 <1lch2@163.com>
2021-03-01 19:48:35 +0800

    updated wrapper types, fixed some typo

--- java/basics/TestVariableTypes.java
+++ java/basics/TestVariableTypes.java
@@ -7,11 +7,11 @@ public class TestVariableTypes {
         // Java 基本类型，赋值时使用传值操作
         // 变量必须先初始化再引用
 
-        // BasicTypes.printInfo(); // 基础类型信息
-        BasicTypes.printArray(); // 打印数组和数组地址
+        BasicTypes.printInfo(); // 基础类型信息
+        // BasicTypes.printArray(); // 打印数组和数组地址
         // BasicTypes.testOverflow(); // 测试整数溢出
         // BasicTypes.testTypeConvert(); // 测试强制类型转换
-        // WrapperTypes.testWrapperTypes(); // 测试包装类型
+        WrapperTypes.testWrapperTypes(); // 测试包装类型
     }
 }
 
@@ -23,6 +23,8 @@ class BasicTypes {
     */
     // 静态方法，不通过实例对象调用
     public static void printInfo() {
+        System.out.println("----------基本类型----------");
+
         // * Java 各基础类型的位数，最大值和最小值
         // * 来源：https://www.runoob.com/java/java-basic-datatypes.html
 
@@ -55,29 +57,33 @@ class BasicTypes {
         System.out.println();
 
         // * float
-        System.out.println("基本类型：float 二进制位数：" + Float.SIZE);
+        System.out.println("基本类型：float 二进制位数：" + Float.SIZE); //32
         System.out.println("包装类：java.lang.Float");
         System.out.println("最小值：Float.MIN_VALUE=" + Float.MIN_VALUE);
         System.out.println("最大值：Float.MAX_VALUE=" + Float.MAX_VALUE);
         System.out.println();
 
         // * double
-        System.out.println("基本类型：double 二进制位数：" + Double.SIZE);
+        System.out.println("基本类型：double 二进制位数：" + Double.SIZE); //64
         System.out.println("包装类：java.lang.Double");
         System.out.println("最小值：Double.MIN_VALUE=" + Double.MIN_VALUE);
         System.out.println("最大值：Double.MAX_VALUE=" + Double.MAX_VALUE);
         System.out.println();
 
         // * char
-        System.out.println("基本类型：char 二进制位数：" + Character.SIZE);
+        System.out.println("基本类型：char 二进制位数：" + Character.SIZE); //16
         System.out.println("包装类：java.lang.Character");
         // 以数值形式而不是字符形式将Character.MIN_VALUE输出到控制台
         System.out.println("最小值：Character.MIN_VALUE=" + (int) Character.MIN_VALUE); // \u0000
         // 以数值形式而不是字符形式将Character.MAX_VALUE输出到控制台
         System.out.println("最大值：Character.MAX_VALUE=" + (int) Character.MAX_VALUE); // \uffff
+        System.out.println();
+
     }
 
     public static void printArray() {
+        System.out.println("----------数组类型----------");
+
         /*
         * 打印数组的多种方法
         */
@@ -95,7 +101,7 @@ class BasicTypes {
 
         String[] stringArray = new String[] {"00", "01", "10", "11"};
         System.out.println("\n对引用类型元素的数组进行赋值，结果是在对应下标连续存放字符串的指针");
-        System.out.println("对数组元素进行修改，结果时在新的内存地址存入字符串，然后修改对应下标的指针");
+        System.out.println("对数组元素进行修改，结果是在新的内存地址存入字符串，然后修改对应下标的指针");
         System.out.println("Address of stringArray: " + stringArray);
 
         String ref = stringArray[0]; // 此处是将对应元素的指针赋值给了 ref 
@@ -173,6 +179,8 @@ class WrapperTypes{
      */
 
     public static void testWrapperTypes() {
+        System.out.println("----------包装类型----------");
+
         /* 
         * Integer 
         */
@@ -186,13 +194,42 @@ class WrapperTypes{
 
         // 一般使用
         Integer numA = Integer.valueOf(10);
-        System.out.print("numA == inNum ?: ");
+        System.out.println("numA == inNum : ");
         System.out.println(numA == intNum); // true
+        System.out.println();
 
         /*        
         * String
         */
-        // TODO：
         
+        // 在创建String对象时，若已有相同的缓存对象，则直接返回引用
+        String str = "string";
+        String str_2 = str;
+
+        System.out.println("Is equal: str == str_2: "); 
+        System.out.println(str == str_2);
+        System.out.println();
+
+        // new 会直接创建一个新对象
+        String new_str = new String("string");
+
+        System.out.println("Is equal: str == new_str: ");
+        System.out.println(str == new_str);
+        System.out.println();
+
+        // 字符串截取
+        System.out.println("原字符串：" + str);
+        System.out.println("使用substring()截取，左开右闭，原理同Python的切片：" + str.substring(1,3));
+
+        // String 本身不可变，使用变量赋值只是更改了指针
+        String s1 = "first";
+        String s2 = s1; // 此时 s2 指向 “first”
+
+        s1 = "second";
+        System.out.println("s2 的引用没有变化，依然指向内存中存储 “first” 的地址\n" + "s2: " + s2);
+
+        // 格式化字符串
+        // 格式化符同C语言
+        String formatted_str = String.format("Name: %s, Age: %d", "John", 24);
     }
 }
