5c57ebdce964dde807421225d5354825705bc6c8
1lch2 <1lch2@163.com>
2021-03-16 20:06:20 +0800

    update io

--- src/java/io/TestIO.java
+++ src/java/io/TestIO.java
@@ -10,6 +10,7 @@ public class TestIO {
         testProperty();
         testTraverse();
         testInputStream();
+        testOutputStream();
     }
 
     public static void testFile() {
@@ -67,17 +68,22 @@ public class TestIO {
     }
 
     public static void testInputStream() throws IOException{
+        //* InputSteram.read() 是阻塞型方法，IO耗时较大，容易影响性能
+
+        System.out.println("使用InputStream每次读取一个字节");
         // 一次读取一个字节
         try(InputStream input = new FileInputStream("src/java/io/sample.txt")) { 
             // try(resource) 语法可以让 JDK 自动添加关闭资源的语句
             // 前提是 resource 实现了 java.lang.AutoCloseable 接口
             int n;
-            while ((n = input.read()) != -1) {
+            while ((n = input.read()) != -1) { // 逐一返回读到的每一个字节
                 System.out.print((char) n);
             }
             System.out.println();
         }
+        System.out.println();
 
+        System.out.println("使用缓冲区每次读取多个字节");
         // 利用缓冲区读取
         try (InputStream input = new FileInputStream("src/java/io/sample.txt")) {
             // 定义1000个字节大小的缓冲区:
@@ -85,8 +91,42 @@ public class TestIO {
             int n;
             while ((n = input.read(buffer)) != -1) { // 读取到缓冲区，此时 n 返回的是读取的字节数
                 System.out.println("read " + n + " bytes.");
-                System.out.println(buffer.toString()); // TODO: print buffer to string
             }
+            for (int i : buffer) {
+                System.out.print((char) i);
+            }
+            System.out.println();
         }
+        System.out.println();
+
+        System.out.println("利用 ByteInputStream 构造InputStream");
+        byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input = new ByteArrayInputStream(data)) {
+            //* 利用抽象的 InputStream 类型，接受抽象类型，使得可以传入任意的 InputStream 实现类
+            int n;
+            while ((n = input.read()) != -1) {
+                System.out.print((char)n);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    public static void testOutputStream() throws IOException{
+        //* OutputStream 是所有输出流的超类，也是一个抽象类
+        System.out.println("使用 FileOutputStream 写入文件流");
+        OutputStream output = new FileOutputStream("src/java/io/output.txt");
+        output.write(97);
+        output.write("\nhello world!".getBytes("UTF-8")); // write() 也是一个阻塞方法
+        output.close();
+        System.out.println();
+
+        // 仅限 Java 9
+        // System.out.println("将数据从输入流传输到输出流");
+        // try (InputStream input = new FileInputStream("src/java/io/sample.txt");
+        //     OutputStream output0 = new FileOutputStream("src/java/io/output.txt")) {
+        //     input.transferTo(output0);
+        // }
     }
 }
