15608df40b0a8bfe60ea6495b99980efb4d3f67c
1lch2 <1lch2@163.com>
2021-06-03 16:58:45 +0800

    updated javapoet sample

---
+++ org/bd/java_exercise/javapoet/Hello.java
@@ -0,0 +1,10 @@
+package org.bd.java_exercise.javapoet;
+
+import java.lang.String;
+import java.lang.System;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("hello world!");
+  }
+}
